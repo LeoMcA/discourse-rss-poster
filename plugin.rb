b39b3ee6cd5ec05d361d8e865cce7164036efedf
Leo McArdle <leomcardle@gmail.com>
2016-09-05 00:54:28 +0100

    improved form styling

--- plugin.rb
+++ plugin.rb
@@ -6,6 +6,8 @@
 load File.expand_path('../lib/rss_poster.rb', __FILE__)
 load File.expand_path('../lib/rss_poster/engine.rb', __FILE__)
 
+register_asset 'stylesheets/rss_poster.scss'
+
 after_initialize do
   load File.expand_path('../jobs/rss_poster_poll.rb', __FILE__)
   RssPoster::Feed.all.each do |feed|
