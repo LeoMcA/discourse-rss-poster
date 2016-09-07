c7796f169a8012646a15abe018c8471487889096
Leo McArdle <leomcardle@gmail.com>
2016-09-07 04:43:07 +0100

    bump version

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: rss-poster
 # about: Discourse plugin which creates posts from feeds
-# version: 0.1
+# version: 0.2
 # authors: Leo McArdle
 # url: https://github.com/LeoMcA/discourse-rss-poster
 
